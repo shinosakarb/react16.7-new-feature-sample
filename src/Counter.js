721ccdb9daf231d70f82ea065a3d8823fd775cdc
chimame <rito.tamata@gmail.com>
2018-12-15 14:30:37 +0900

    Add useReducer

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
+import { reducer, initialState } from './reducers/Counter'
 
 export default () => {
-  const [count, setCount] = useState(0)
+  const [state, dispatcher] = useReducer(reducer, initialState)
 
   useEffect(() => {console.log('changed!')})
-  useEffect(() => {document.title = `Count: ${count}`})
+  useEffect(() => {document.title = `Count: ${state.count}`})
 
   return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(0)}>reset</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatcher({ type: 'reset' })}>reset</button>
+      <button onClick={() => dispatcher({ type: 'decrement' })}>-</button>
+      <button onClick={() => dispatcher({ type: 'increment' })}>+</button>
     </div>
   )
 }

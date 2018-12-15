6afcc041527a4ad0dd4b22ee3ee61610d0ee482a
chimame <rito.tamata@gmail.com>
2018-12-15 13:57:38 +0900

    Change count state by useState

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default class Counter extends React.Component {
-  state = {count: 0}
+export default () => {
+  const [count, setCount] = useState(0)
 
-  render() {
-    return (
-      <div>
-        <p>Count: {this.state.count}</p>
-        <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
-        <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  )
 }
